37426b70495c63dae7ef84812bd59863da5b1f46
aniketroy420 <aniketroy2002@gmail.com>
2023-06-27 16:31:08 +0530

    screenshots and demo

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -10,8 +10,24 @@ class SplashScreen extends StatelessWidget {
         decoration: const BoxDecoration(
             image: DecorationImage(
                 image: AssetImage('assets/images/top.gif'), fit: BoxFit.fill)),
-        child: const Center(
-          child: Text('loading...'),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+
+            Container(
+              margin: const EdgeInsets.only(
+                top: 30,
+                bottom: 20,
+                left: 20,
+                right: 20,
+              ),
+              width: 200,
+              child: Image.asset('assets/images/main.png',
+                  opacity: const AlwaysStoppedAnimation(.6)),
+            ),
+               CircularProgressIndicator(color: Colors.white.withOpacity(0.6),),
+          ]),
         ),
       ),
     );
